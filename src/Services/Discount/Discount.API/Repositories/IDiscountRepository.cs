64a97eaeae4bf7cba3056f7478fe2213c302d510
firatkeler <firatkeler@gmail.com>
2021-12-14 19:49:41 +0300

    Discount.API

---
+++ src/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discount.API.Entities;
+
+namespace Discount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string ProductName);
+
+        Task<bool> AddDiscount(Coupon coupon);
+
+        Task<bool> UpdateDiscount(Coupon coupon);
+
+        Task<bool> DeleteDiscount(string ProductName);
+    }
+}
