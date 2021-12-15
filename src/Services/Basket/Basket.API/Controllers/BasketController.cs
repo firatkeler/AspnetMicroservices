9d5885a33dd815a920bbafbe86f3c5167462a194
firatkeler <firatkeler@gmail.com>
2021-12-15 22:08:44 +0300

    Discount.Grpc

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Threading.Tasks;
 using Basket.API.Entities;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -13,10 +14,12 @@ namespace Basket.API.Controllers
     public class BasketController: ControllerBase
     {
         private readonly IBasketRepository _repository;
+        private readonly DiscountGrpcService _discountGrpcService;
 
-        public BasketController(IBasketRepository repository, ILogger<BasketController> logger)
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
         }
 
         [HttpGet("{userName}", Name = "GetBasket")]
@@ -32,6 +35,15 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket) 
         {
+            foreach (var item in basket.Items)
+            {
+
+                var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+
+                item.Price -= coupon.Amount;
+            }
+
+
             return Ok(await _repository.UpdateBasket(basket));
         }
 
---
+++ src/Services/Discount/Discount.Grpc/Repositories/IDiscountRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discount.Grpc.Entities;
+
+namespace Discount.Grpc.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string ProductName);
+
+        Task<bool> AddDiscount(Coupon coupon);
+
+        Task<bool> UpdateDiscount(Coupon coupon);
+
+        Task<bool> DeleteDiscount(string ProductName);
+    }
+}
