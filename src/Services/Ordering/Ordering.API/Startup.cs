d3206430f28edcb934bf2443d4db2fd440c71e63
firatkeler <firatkeler@gmail.com>
2023-04-30 01:11:58 +0300

    Ordering Infrastructure Migrations

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Ordering.Application;
+using Ordering.Infrastructure;
 
 namespace Ordering.API
 {
@@ -25,7 +27,8 @@ namespace Ordering.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddApplicationServices();
+            services.AddInfrastructureServices(Configuration);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
